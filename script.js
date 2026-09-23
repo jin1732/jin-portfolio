4ed630ad97374a843ccaf3dfce29c81d27d9a863
손희진 <son1732321732@c6r2s8.codyssey.kr>
2026-09-23 16:32:20 +0900

    깃허브 연동

--- script.js
+++ script.js
@@ -7,27 +7,41 @@ async function getRepos() {
         const response = await fetch(apiUrl);
         const repos = await response.json();
 
-        console.log(repos);
+        // 로딩 메시지 숨기기
+        const loadingElement = document.getElementById('loading');
+        if (loadingElement) {loadingElement.remove();
+        }
+
         displayRepos(repos);
     } catch (error) {
-        console.error("데이타를 가져오는 중 에러 발생:", error);
+        console.error("에러 발생:", error);
+        document.getElementById('loading').innerText = "데이타불러오지 못했습니다.";
     }
 }
 
 // 화면에 프로젝트를 그려주는 함수
 function displayRepos(repos) {
     const projectGrid = document.getElementById('project-grid');
+    const loadingElement = document.getElementById('loading');
+
+    if (loadingElement) loadingElement.style.display = 'none';
+
     projectGrid.innerHTML = '';
 
     repos.forEach(repo => {
+        // 2. 새로운 div(카드) 생성
         const card = document.createElement('div');
+        // ★ 중요: 여기서 HTML에는 없던 'project-card' 클래스를 부여합니다!
         card.className = 'project-card';
+
+        // 3. 카드 내부에 들어갈 내용 작성
         card.innerHTML = `
             <h3>${repo.name}<h3>
             <p>${repo.description || '설명이 없습니다.'}<p>
-            <a href="${repo.html_url}"target="_blank">자세히 보기</a>
+            <a href="${repo.html_url}" target="_blank">자세히 보기</a>
         `;
-        projectGrid.appendChild(card)
+        // 4. 완성된 카드를 project-grid 안에 넣기
+        projectGrid.appendChild(card);
     });
 }
 getRepos()
